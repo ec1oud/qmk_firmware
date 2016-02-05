fc668216c0089f648f97a654acf70b0f60d72e36
Shawn Rutledge <s@ecloud.org>
2016-02-05 20:07:13 +0100

    give up on ctrl/esc for now; other changes as on Infinity Ergodox

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `/~  |   1  |   2  |   3  |   4  |   5  |   6  |           |   *  |   7  |   8  |   9  |   0  |  \ | |  = +   |
+ * |   `/~  |   1  |   2  |   3  |   4  |   5  |   6  |           | Func |   7  |   8  |   9  |   0  |  \ | |  = +   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  up  |           | down |   Y  |   U  |   I  |   O  |   P  |  - _   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Left |           | Right|------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  / ? | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Func |WinCmd| Hyper| Meh  | Alt  |                                       |   (  |  )   |  [   |   ]  | Ctrl |
+ *   | Func |WinCmd|  *   |   &  | Alt  |                                       |   (  |  )   |  [   |   ]  | Ctrl |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | Home | Ins  |       | PgUp |  End   |
@@ -33,18 +33,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LEFT,
-        CTL_T(KC_ESC),  KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LCTL,  		KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_RGHT,
-        KC_FN1,       KC_LGUI,   ALL_T(KC_NO),MEH_T(KC_NO), KC_LALT,
+        KC_FN1,       KC_LGUI,        KC_ASTR,KC_AMPR, KC_LALT,
                                               KC_HOME,KC_INS,
                                                               KC_DEL,
-                                               KC_SPC,KC_BSPC,KC_FN1,
+                                               KC_SPC,KC_BSPC,KC_ESC,
         // right hand
              TG(1),      KC_7,   KC_8,   KC_9,   KC_0,   KC_BSLS,        KC_EQL,
              KC_UP,      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,           KC_MINS,
                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,		 KC_QUOT,
              KC_DOWN,    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,        KC_RSFT,
-                                  KC_LPRN,KC_RPRN,KC_LBRC,KC_RBRC,       CTL_T(KC_NO),
+                                 KC_LPRN,KC_RPRN,KC_LBRC,KC_RBRC,       KC_RCTL,
              KC_PGUP,        KC_END,
              KC_PGDN,
              KC_RALT, KC_BSPC, KC_ENT
@@ -73,22 +73,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // FUNCTION MEDIA AND MOUSE
 [FNMM] = KEYMAP(
        KC_F14,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_HOME,
        KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END,
+       KC_TRNS, KC_TRNS, KC_AT,   KC_EXLM, KC_TRNS,
                                            KC_TRNS, KC_TRNS,
                                                     KC_TRNS,
-                                  KC_TRNS, KC_TRNS, KC_TRNS,
+                                  KC_BTN1, KC_BTN2, KC_SLCK,
     // right hand
        KC_TRNS,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-       KC_MUTE, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,  KC_TRNS, KC_MPLY,
+       KC_PGUP, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,  KC_MUTE, KC_MPLY,
                  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS , KC_TRNS,
-       KC_BTN3,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_UP,   KC_TRNS,
+       KC_PGDN,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_UP,   KC_TRNS,
                           KC_BTN1, KC_BTN2, KC_LEFT, KC_DOWN, KC_RGHT,
-       KC_TRNS, KC_TRNS,
+       KC_PSCR, KC_TRNS,
        KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
+       KC_RGUI, KC_TRNS, KC_TRNS
 ),
 };
 
