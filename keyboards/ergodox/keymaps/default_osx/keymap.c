c949d177964544fec6c2cb0207cd1d4828fa5bc7
Shawn Rutledge <s@ecloud.org>
2016-06-09 07:41:37 +0200

    move equal and backslash to be more qwerty-like
    As on the Infinity Ergdox, did this to be able to use more pre-printed
    keycaps, and because I type on regular QWERTY keyboards sometimes too.

--- keyboards/ergodox/keymaps/default_osx/keymap.c
+++ keyboards/ergodox/keymaps/default_osx/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `/~  |   1  |   2  |   3  |   4  |   5  |   6  |           | Func |   7  |   8  |   9  |   0  |  \ | |  = +   |
+ * |   `/~  |   1  |   2  |   3  |   4  |   5  |   6  |           | Func |   7  |   8  |   9  |   0  |  = + |   \ |  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  up  |           | down |   Y  |   U  |   I  |   O  |   P  |  - _   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               KC_DEL,
                                                KC_SPC,KC_BSPC,KC_ESC,
         // right hand
-             TG(1),      KC_7,   KC_8,   KC_9,   KC_0,   KC_BSLS,        KC_EQL,
+             TG(1),      KC_7,   KC_8,   KC_9,   KC_0,   KC_EQL,         KC_BSLS,
              KC_UP,      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,           KC_MINS,
                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,		 KC_QUOT,
              KC_DOWN,    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,        KC_RSFT,
