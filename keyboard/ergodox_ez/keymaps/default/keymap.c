27f7132f39c1dcf2b1568fd98c99e927794cca19
Shawn Rutledge <s@ecloud.org>
2016-07-18 11:14:55 +0200

    no command mode; move tilde left of 7, fn1 left of 1

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `/~  |   1  |   2  |   3  |   4  |   5  |   6  |           | Func |   7  |   8  |   9  |   0  |  = + |   \ |  |
+ * | Func   |   1  |   2  |   3  |   4  |   5  |   6  |           |  `/~ |   7  |   8  |   9  |   0  |  = + |   \ |  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  up  |           | down |   Y  |   U  |   I  |   O  |   P  |  - _   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
+        KC_FN2,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LEFT,
         KC_LCTL,  		KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_RGHT,
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               KC_DEL,
                                                KC_SPC,KC_BSPC,KC_ESC,
         // right hand
-             TG(1),      KC_7,   KC_8,   KC_9,   KC_0,   KC_EQL,         KC_BSLS,
+             KC_GRV,     KC_7,   KC_8,   KC_9,   KC_0,   KC_EQL,         KC_BSLS,
              KC_UP,      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,           KC_MINS,
                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,		 KC_QUOT,
              KC_DOWN,    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,        KC_RSFT,
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 2: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  euro  |  F1  |  F2  |  F3  |  F4  |  F5  | euro |           |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |   F12  |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |      |  F7  |  F8  |  F9  |  F10 |  F11 |   F12  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      | MsUp |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // FUNCTION MEDIA AND MOUSE
 [FNMM] = KEYMAP(
-       KC_F14,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
+       KC_TRNS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_HOME,
        KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END,
@@ -93,7 +93,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(FNMM)                // FN1 - Momentary Layer 1
+    [1] = ACTION_LAYER_TAP_TOGGLE(FNMM),               // FN1 - Toggle Layer 1
+    [2] = ACTION_LAYER_MOMENTARY(FNMM)                 // FN2 - Momentary Layer 1
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
